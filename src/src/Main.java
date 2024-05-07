e208130af65eb0234ff76675fa032c74c7fcf2d2
FabianAngelesCS <cruzfabian292@gmail.com>
2024-05-07 17:31:08 -0600

    agregando mejoraa

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,26 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Producto[] productos = new Producto[5];
 
-        Accesorio esclava = new Accesorio("esclava", 1200, 3, "lujo");
-        Prenda pantalon = new Prenda("pantalon", 800, 5, 'M');
-        Prenda camisa = new Prenda("camisa", 400, 7, 'M');
-        Accesorio gorra = new Accesorio("gorra", 600, 3, "moda");
-
-        productos[0] = esclava;
-        productos[1] = pantalon;
-        productos[2] = camisa;
-        productos[3] = gorra;
-
-
-        for (Producto producto: productos){
-            System.out.println("Nombre: " + producto.getNombre() +
-                    " -------Cantidad: " + producto.getCantidad() +
-                    " -------precio: $" + producto.getPrecio());
-
-
-        }
+        System.out.println("Hola, este el el almacen de tu tienda");
+        System.out.println("¿Qué deseas realizar? ");
+        System.out.println("A -----> Ver inventario");
+        System.out.println("B -----> Agregar productos");
 
 
     }
---
+++ src/src/Tienda.java
@@ -0,0 +1,21 @@
+public class Tienda {
+
+    private Producto[] inventario;
+
+    private int cantidadProductos;
+
+    public Tienda(int  capacidadMaxima) {
+        this.inventario = new Producto[capacidadMaxima]
+        this.cantidadProductos = 0;
+    }
+
+    public void agregarProductos(Producto producto){
+
+        if (cantidadProductos < inventario.length){
+            inventario[cantidadProductos] = producto;
+            cantidadProductos++;
+            System.out.println("El siguiete producto fue agregado correctamente: " + producto.getNombre());
+        }
+    }
+
+}
